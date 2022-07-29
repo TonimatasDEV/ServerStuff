f964bf36449271dec438d15e06af108897fcd285
TonimatasMC <amatascubero@gmail.com>
2022-07-29 18:03:08 +0200

    TabCompleter

---
+++ src/main/java/com/danizml/tabCompleter/TabCompleter.java
@@ -0,0 +1,13 @@
+package com.danizml.tabCompleter;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+public class TabCompleter implements org.bukkit.command.TabCompleter {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return null;
+    }
+}
---
+++ src/main/java/net/tonimatasmc/tabCompleter/TabCompleter.java
@@ -0,0 +1,13 @@
+package net.tonimatasmc.tabCompleter;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+public class TabCompleter implements org.bukkit.command.TabCompleter {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return null;
+    }
+}
