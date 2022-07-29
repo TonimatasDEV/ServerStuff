7d0c0974d04c4216d3bf7cec097feeec20952987
TonimatasMC <amatascubero@gmail.com>
2022-07-29 18:03:22 +0200

    Message variables

---
+++ src/main/java/net/tonimatasmc/util/Message.java
@@ -0,0 +1,15 @@
+package net.tonimatasmc.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public class Message {
+    public static void sendToPlayer(Player player, String message) {
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+    }
+
+    public static void sendToConsole(String message) {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+    }
+}
