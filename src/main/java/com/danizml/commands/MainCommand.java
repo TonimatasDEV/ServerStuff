bc246a619e4ee4ddf88de70a843a869436e71b7d
TonimatasMC <amatascubero@gmail.com>
2022-07-30 17:10:56 +0200

    Fixed syntax error

--- src/main/java/com/danizml/commands/MainCommand.java
+++ src/main/java/com/danizml/commands/MainCommand.java
@@ -14,31 +14,34 @@ public class MainCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("serverstuff")) {
-            if (args[0].equalsIgnoreCase("help")) {
-                if (sender.hasPermission("serverstuff.help")) {
+            if (args.length >= 2) {
+                if (args[0].equalsIgnoreCase("help")) {
+                    if (sender.hasPermission("serverstuff.help")) {
 
-                    //help message
-                    MessageColor.sendToPlayer((Player) sender, "&6ServerStuff &8&l> &6Help");
-                    MessageColor.sendToPlayer((Player) sender, "&8&l[&5+&8&l] &5/serverstuff help");
-                    MessageColor.sendToPlayer((Player) sender, "&8&l[&5+&8&l] &5/serverstuff reload");
+                        //help message
+                        MessageColor.sendToPlayer((Player) sender, "&6ServerStuff &8&l> &6Help");
+                        MessageColor.sendToPlayer((Player) sender, "&8&l[&5+&8&l] &5/serverstuff help");
+                        MessageColor.sendToPlayer((Player) sender, "&8&l[&5+&8&l] &5/serverstuff reload");
 
-                } else {
-                    //Class "PermissionMessage"
-                    PermissionMessage.nonPermission((Player) sender);
+                    } else {
+                        //Class "PermissionMessage"
+                        PermissionMessage.nonPermission((Player) sender);
+                    }
+
+                } else if (args[0].equalsIgnoreCase("reload")) {
+                    if (sender.hasPermission("serverstuff.reload")) {
+
+                        //reload plugin
+                        ServerStuff.getInstance().reloadConfig();
+                        Messages.reloadMessages();
+                        MessageColor.sendToPlayer((Player) sender, "&6ServerStuff &8&l> &9Plugin Reloaded");
+
+                    } else {
+                        PermissionMessage.nonPermission((Player) sender);
+                    }
                 }
-
-
-            } else if (args[0].equalsIgnoreCase("reload")) {
-                if (sender.hasPermission("serverstuff.reload")) {
-
-                    //reload plugin
-                    ServerStuff.getInstance().reloadConfig();
-                    Messages.reloadMessages();
-                    MessageColor.sendToPlayer((Player) sender, "&6ServerStuff &8&l> &9Plugin Reloaded");
-
-                } else {
-                    PermissionMessage.nonPermission((Player) sender);
-                }
+            } else {
+                MessageColor.sendToPlayer((Player) sender, "&4[Error]: &7Syntax error.");
             }
         }
         return false;
