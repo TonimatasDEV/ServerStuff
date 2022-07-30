28b7b3c0e4344e125c3a83584bc87b90e1d27abd
danizml <josedanielmorebolopez@gmail.com>
2022-07-30 03:28:18 +0200

    variables and gitignore

--- src/main/java/com/danizml/core/Variables.java
+++ src/main/java/com/danizml/core/Variables.java
@@ -1,19 +1,11 @@
 package com.danizml.core;
 
-
 import org.bukkit.Bukkit;
 import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitTask;
-
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class Variables {
-    public static Map<Player, BukkitTask> tasks = new HashMap<>();
-
-    public static void sendBroadcastMessage(Player player, String message) {
+    public static void sendWithReplacement(Player player, String message) {
 
         //Variables
         Bukkit.broadcastMessage(message.replaceAll("%xp%", String.valueOf(player.getExp())));
@@ -25,5 +17,8 @@ public class Variables {
         Bukkit.broadcastMessage(message.replaceAll("%player_kills%", String.valueOf(player.getStatistic(Statistic.PLAYER_KILLS))));
         Bukkit.broadcastMessage(message.replaceAll("%player_deaths%", String.valueOf(player.getStatistic(Statistic.DEATHS))));
         Bukkit.broadcastMessage(message.replaceAll("%mob_kills%", String.valueOf(player.getStatistic(Statistic.MOB_KILLS))));
+        Bukkit.broadcastMessage(message.replaceAll("%fish_caught%", String.valueOf(player.getStatistic(Statistic.FISH_CAUGHT))));
+        Bukkit.broadcastMessage(message.replaceAll("%jumps%", String.valueOf(player.getStatistic(Statistic.JUMP))));
+        Bukkit.broadcastMessage(message.replaceAll("%mien_blocks%", String.valueOf(player.getStatistic(Statistic.MINE_BLOCK))));
     }
 }
