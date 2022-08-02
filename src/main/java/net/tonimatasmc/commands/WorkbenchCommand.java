7e891ac09a85e2830c0f662a39f17c83cbbd579d
TonimatasMC <amatascubero@gmail.com>
2022-08-02 20:39:33 +0200

    WorkbenchCommand

---
+++ src/main/java/net/tonimatasmc/commands/WorkbenchCommand.java
@@ -0,0 +1,38 @@
+package net.tonimatasmc.commands;
+
+import main.serverstuff.ServerStuff;
+import net.tonimatasmc.util.UsedMessages;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class WorkbenchCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("workbench") || command.getName().equalsIgnoreCase("wb")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("[&4Error&f]: You not a player.");
+            } else {
+                Player player = (Player) sender;
+
+                if (player.hasPermission("serverstuff.workbench")) {
+                    if (args.length == 1) {
+                        Player target = ServerStuff.getInstance().getServer().getPlayer(args[0]);
+
+                        if (target.isOnline()) {
+                            target.openWorkbench(player.getLocation(), true);
+                        } else {
+                            UsedMessages.targetNotOnline(target);
+                        }
+                    } else {
+                        player.openWorkbench(player.getLocation(), true);
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
--- src/main/java/net/tonimatasmc/managers/CommandManager.java
+++ src/main/java/net/tonimatasmc/managers/CommandManager.java
@@ -3,6 +3,7 @@ package net.tonimatasmc.managers;
 import net.tonimatasmc.commands.EnableDisablePluginsCommand;
 import net.tonimatasmc.commands.SpeedCommand;
 import net.tonimatasmc.commands.TPOfflinePlayerCommand;
+import net.tonimatasmc.commands.WorkbenchCommand;
 import net.tonimatasmc.tabCompleter.TabCompleter;
 import org.bukkit.Bukkit;
 
@@ -16,5 +17,7 @@ public class CommandManager {
 
         Bukkit.getServer().getPluginCommand("tpoffline").setExecutor(new TPOfflinePlayerCommand());
         Bukkit.getServer().getPluginCommand("tpoffline").setTabCompleter(new TabCompleter());
+
+        Bukkit.getServer().getPluginCommand("workbench").setExecutor(new WorkbenchCommand());
     }
 }
